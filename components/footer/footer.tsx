bb3e17bc5052a0b271da8c4fa03ce54f719c4b64
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2023-01-22 17:04:43 +0100

    footer wip

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,7 +4,27 @@ const Footer = () => {
   return (
     <footer className="footer h-auto bg-base-200 border-t-[1px] absolute bottom-0">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <nav className="flex title-font font-medium items-center md:justify-start justify-center">
+        <nav className="flex title-font font-medium items-center justify-center sm:hidden">
+          <Link href="/" className="font-title antialiasing text-xl350">
+            Home
+          </Link>
+          <Link
+            href="/statistiche"
+            className="mx-3 font-title antialiasing text-xl350"
+          >
+            Statistiche
+          </Link>
+          <Link
+            href="/previsioni"
+            className="font-title antialiasing text-xl350"
+          >
+            Previsioni
+          </Link>
+        </nav>
+        <a className="flex title-font font-medium items-center md:justify-start justify-center">
+          <span className="ml-3 text-xl">Travel Trend</span>
+        </a>
+        <nav className="hidden flex title-font font-medium items-center justify-center mx-auto sm:inline-block">
           <Link
             href="/"
             className="font-title antialiasing text-xl350"
@@ -24,11 +44,8 @@ const Footer = () => {
             Previsioni
           </Link>
         </nav>
-        <a className="flex title-font font-medium items-center md:justify-start justify-center">
-          <span className="ml-3 text-xl">Travel Trend</span>
-        </a>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a>
+          <a href="https://facebook.com" rel="nofollow">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -40,7 +57,7 @@ const Footer = () => {
               <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
             </svg>
           </a>
-          <a className="ml-3 ">
+          <a className="ml-3" href="https://twitter.com" rel="nofollow">
             <svg
               fill="currentColor"
               strokeLinecap="round"
@@ -52,7 +69,7 @@ const Footer = () => {
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
           </a>
-          <a className="ml-3 ">
+          <a className="ml-3" href="https://instagram.com" rel="nofollow">
             <svg
               fill="none"
               stroke="currentColor"
@@ -66,7 +83,7 @@ const Footer = () => {
               <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
             </svg>
           </a>
-          <a className="ml-3 ">
+          <a className="ml-3" href="https://linkedin.com" rel="nofollow">
             <svg
               fill="currentColor"
               stroke="currentColor"
