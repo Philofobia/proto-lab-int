6e584f0583de353ecff8a81da6d45bd8ef598bd4
Marco Parisi <marco.parisi@edu.itspiemonte.it>
2023-01-26 09:30:16 +0100

    added favicon

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -7,97 +7,97 @@ const Header = ({ navigationData }: { navigationData: RegioneInt[] }) => {
   const { navbar, switchNavbar } = NavbarConsumer();
 
   return (
-      <header className="navbar bg-base-100 shadow-xl fixed z-50 top-0 h-12 lg:px-5">
-        <section className="navbar-start lg:hidden">
-          <button className="btn btn-ghost" onClick={switchNavbar}>
-            {navbar ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block w-6 h-6 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            )}
-          </button>
-        </section>
+    <header className="navbar bg-base-100 shadow-xl fixed z-50 top-0 h-12 lg:px-5">
+      <section className="navbar-start lg:hidden">
+        <button className="btn btn-ghost" onClick={switchNavbar}>
+          {navbar ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-6 h-6 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          )}
+        </button>
+      </section>
 
-        <nav className="navbar-start hidden lg:inline-block">
-          <Link
-            href="/"
+      <nav className="navbar-start hidden lg:inline-block">
+        <Link
+          href="/"
+          className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
+        >
+          Home
+        </Link>
+        <Link
+          href="/statistiche"
+          className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
+        >
+          Statistiche
+        </Link>
+        <Link
+          href="/previsioni"
+          className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
+        >
+          Previsioni
+        </Link>
+        <div className="dropdown dropdown-bottom">
+          <label
+            tabIndex={0}
             className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
           >
-            Home
-          </Link>
-          <Link
-            href="/statistiche"
-            className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
+            Regioni
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
           >
-            Statistiche
-          </Link>
-          <Link
-            href="/previsioni"
-            className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
-          >
-            Previsioni
-          </Link>
-          <div className="dropdown dropdown-bottom">
-            <label
-              tabIndex={0}
-              className="mr-5 font-title antialiasing text-xl350 hover:text-primary"
-            >
-              Regioni
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52"
-            >
-              {navigationData.map((regione, index) => (
-                <li key={index}>
-                  <Link href={`/${regione.regione.nomeRegione}`}>
-                    <strong>{regione.regione.nomeRegione}</strong>
+            {navigationData.map((regione, index) => (
+              <li key={index}>
+                <Link href={`/${regione.regione.nomeRegione}`}>
+                  <strong>{regione.regione.nomeRegione}</strong>
+                </Link>
+              </li>
+            ))}
+            {navigationData.map((regione) =>
+              regione.province.map((prov, index) => (
+                <li key={index} className="ml-2">
+                  <Link href={`/${regione.regione.nomeRegione}/${prov}`}>
+                    {prov}
                   </Link>
                 </li>
-              ))}
-              {navigationData.map((regione) =>
-                regione.province.map((prov, index) => (
-                  <li key={index} className="ml-2">
-                    <Link href={`/${regione.regione.nomeRegione}/${prov}`}>
-                      {prov}
-                    </Link>
-                  </li>
-                ))
-              )}
-            </ul>
-          </div>
-        </nav>
-        <section className="navbar-center">
-          <h2 className="font-title antialiasing text-2xl">TRAVEL TREND</h2>
-        </section>
-        <section className="navbar-end pr-3">
-          <ThemeButton />
-        </section>
+              ))
+            )}
+          </ul>
+        </div>
+      </nav>
+      <section className="navbar-center">
+        <h2 className="font-title antialiasing text-2xl">TRAVEL TREND</h2>
+      </section>
+      <section className="navbar-end pr-3">
+        <ThemeButton />
+      </section>
     </header>
   );
 };
--- pages/previsioni/index.tsx
+++ pages/previsioni/index.tsx
@@ -200,7 +200,7 @@ ChartPrevisioni.getLayout = function PageLayout(page: ReactElement) {
           content="Travel Trend: statistiche e analisi viaggi"
         />
         <meta property="og:url" content="index.html" />
-        <meta property="og:image" content="DA AGGIORNARE" />
+        <meta property="og:image" content="/favicon/travel-trend-logo-96x96.png" />
         <meta
           property="og:title"
           content="Travel Trend: statistiche e analisi viaggi."
@@ -228,34 +228,34 @@ ChartPrevisioni.getLayout = function PageLayout(page: ReactElement) {
         <meta name="robots" content="index, follow" />
         <meta name="author" content="Parisi Marco" />
 
-        <link rel="apple-touch-icon" sizes="57x57" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="60x60" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="72x72" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="76x76" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="114x114" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="120x120" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="144x144" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="152x152" href="Da aggiornare" />
-        <link rel="apple-touch-icon" sizes="180x180" href="Da aggiornare" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/favicon/travel-trend-logo-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/favicon/travel-trend-logo-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/favicon/travel-trend-logo-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/favicon/travel-trend-logo-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/favicon/travel-trend-logo-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/favicon/travel-trend-logo-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/favicon/travel-trend-logo-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/favicon/travel-trend-logo-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/travel-trend-logo-180x180.png" />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="Da aggiornare"
+          href="/favicon/travel-trend-logo-192x192.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="144x144"
-          href="Da aggiornare"
+          href="/favicon/travel-trend-logo-144x144.png"
         />
-        <link rel="icon" type="image/png" sizes="96x96" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="72x72" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="48x48" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="36x36" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="32x32" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="96x96" href="Da aggiornare" />
-        <link rel="icon" type="image/png" sizes="16x16" href="Da aggiornare" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/travel-trend-logo-96x96.png" />
+        <link rel="icon" type="image/png" sizes="72x72" href="/favicon/travel-trend-logo-72x72.png" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon/travel-trend-logo-48x48.png" />
+        <link rel="icon" type="image/png" sizes="36x36" href="/favicon/travel-trend-logo-36x36.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/travel-trend-logo-32x32.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon/travel-trend-logo-96x96.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/travel-trend-logo-16x16.png" />
       </Head>
       {page}
     </>
